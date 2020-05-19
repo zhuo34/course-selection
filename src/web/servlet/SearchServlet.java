3290614fcf7d59717df29147df39912fe24cc0ce
Zhuo Chen <chenzhuo757@zju.edu.cn>
2020-05-19 17:08:17 +0800

    feat: init project with hello world

---
+++ src/web/servlet/SearchServlet.java
@@ -0,0 +1,22 @@
+package web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class SearchServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+		// 写入响应:
+		pw.write("<h1>Hello, world!</h1>");
+		// 最后不要忘记flush强制输出:
+		pw.flush();
+	}
+}
