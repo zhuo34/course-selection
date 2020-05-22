8c03765cc6f5cd1158f6a058bbf322405f9739a9
yuanyike <3170105392@zju.edu.cn>
2020-05-22 15:37:21 +0800

    feat: add a table in ProgramView and add a method to delete a row from table.

--- src/components/ProgramView.vue
+++ src/components/ProgramView.vue
@@ -1,12 +1,50 @@
 <template>
     <div>
-      培养方案
+      <el-table :data="tableData" border="true" style="width: 80%" height="250">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名"  width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+        <el-table-column prop="state" label="">
+          <template slot-scope="scope">
+            <el-button type="info" @click="delTableRow(scope.$index)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <p>{{message}}</p>
     </div>
 </template>
 
 <script>
 export default {
-  name: "ProgramView"
+  name: "ProgramView",
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      message: 'aaa'
+    }
+  },
+  methods: {
+    delTableRow: function(index){
+      this.tableData.splice(index, 1)
+      this.message = 'delete finished.'
+    }
+  }
 }
 </script>
 
