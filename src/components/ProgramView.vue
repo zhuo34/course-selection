e21c1605c3ba72bd41ec71fe384274dff8c0b10d
yuanyike <3170105392@zju.edu.cn>
2020-05-23 17:11:59 +0800

    feat: modify ProgramView.

--- src/components/ProgramView.vue
+++ src/components/ProgramView.vue
@@ -1,16 +1,75 @@
 <template>
     <div>
-      <el-table :data="tableData" border style="width: 80%" height="250">
-        <el-table-column prop="date" label="日期" width="180"></el-table-column>
-        <el-table-column prop="name" label="姓名"  width="180"></el-table-column>
-        <el-table-column prop="address" label="地址"></el-table-column>
-        <el-table-column prop="state" label="">
+      <el-row  type="flex" justify="center">
+        <p>必修课程  已选学分:</p>
+        <p>{{requiredCredit}}</p>
+      </el-row>
+
+      <el-table :data="requiredCourse" border style="width: 80%" height="250">
+        <el-table-column prop="id" label="课程号" width="180"></el-table-column>
+        <el-table-column prop="name" label="课程名称"  width="180"></el-table-column>
+        <el-table-column prop="credit" label="学分"></el-table-column>
+        <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button type="info" @click="delTableRow(scope.$index)">删除</el-button>
+            <el-button type="info" @click="delTableRow(scope.$index, 'requiredCourse')">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
-      <p>{{message}}</p>
+
+      <el-row type="flex" justify="center">
+        <p>选修课程 已选学分：</p>
+        <p>{{selectiveCredit}}</p>
+        <el-button v-popover:tablepop type="primary" round>添加课程</el-button>
+        <el-popover ref="tablepop" placement="bottom-start" trigger="click">
+
+          <el-select v-model="searchTitle1" placeholder="请选择">
+            <el-option v-for="item in searchTitleOptions" :key="item"
+                    :label="item" :value="item"></el-option>
+          </el-select>
+
+          <el-input class="inline-input" v-model="searchInfo1"
+                  prefix-icon="el-icon-search" clearable placeholder="请输入内容"/>
+
+          <el-table :data="courseList" style="width: 100%" border>
+            <el-table-column prop="id" label="课程号"/>
+            <el-table-column prop="name" label="课程名称"/>
+            <el-table-column prop="credit" label="学分"/>
+            <el-table-column prop="op" label="操作">
+              <template slot-scope="scope">
+                <el-button type="danger"
+                  @click="addCourse(scope.$index, 'selectiveCourse')">添加</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-popover>
+      </el-row>
+
+      <el-table :data="selectiveCourse" border style="width: 80%" height="250">
+        <el-table-column prop="id" label="课程号" width="180"></el-table-column>
+        <el-table-column prop="name" label="课程名称"  width="180"></el-table-column>
+        <el-table-column prop="credit" label="学分"></el-table-column>
+        <el-table-column prop="state" label="操作">
+          <template slot-scope="scope">
+            <el-button type="info" @click="delTableRow(scope.$index, 'selectiveCourse')">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <el-row type="flex" justify="center">
+        <p>公共课程 已选学分：</p>
+        <p>{{commonCredit}}</p>
+      </el-row>
+
+      <el-table :data="commonCourse" border style="width: 80%" height="250">
+        <el-table-column prop="id" label="课程号" width="180"></el-table-column>
+        <el-table-column prop="name" label="课程名称"  width="180"></el-table-column>
+        <el-table-column prop="credit" label="学分"></el-table-column>
+        <el-table-column prop="state" label="操作">
+          <template slot-scope="scope">
+            <el-button type="info" @click="delTableRow(scope.$index, 'commonCourse')">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
 </template>
 
@@ -19,30 +78,110 @@ export default {
   name: 'ProgramView',
   data () {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
+      requiredCourse: [{
+        id: '21120261',
+        name: '软件工程',
+        credit: 2.5
       }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
+        id: '21120262',
+        name: '软件工程',
+        credit: 2.5
       }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
+        id: '21120263',
+        name: '软件工程',
+        credit: 2.5
       }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
+        id: '21120264',
+        name: '软件工程',
+        credit: 2.5
       }],
-      message: 'aaa'
+      selectiveCourse: [{
+        id: '21120261',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120262',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120263',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120264',
+        name: '软件工程',
+        credit: 2.5
+      }],
+      commonCourse: [{
+        id: '21120261',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120262',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120263',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120264',
+        name: '软件工程',
+        credit: 2.5
+      }],
+      courseList: [{
+        id: '21120261',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120262',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120263',
+        name: '软件工程',
+        credit: 2.5
+      }, {
+        id: '21120264',
+        name: '软件工程',
+        credit: 2.5
+      }]
+    }
+  },
+  computed: {
+    requiredCredit: function () {
+      let sum = 0
+      for (let i = 0; i < this.requiredCourse.length; i++) {
+        sum = sum + this.requiredCourse[i].credit
+      }
+      return sum
+    },
+    selectiveCredit: function () {
+      let su = 0
+      for (let i = 0; i < this.selectiveCourse.length; i++) {
+        su = su + this.selectiveCourse[i].credit
+      }
+      return su
+    },
+    commonCredit: function () {
+      let s = 0
+      for (let i = 0; i < this.commonCourse.length; i++) {
+        s = s + this.commonCourse[i].credit
+      }
+      return s
     }
   },
   methods: {
-    delTableRow: function (index) {
-      this.tableData.splice(index, 1)
-      this.message = 'delete finished.'
+    delTableRow: function (index, tableName) {
+      if (tableName === 'requiredCourse') {
+        this.requiredCourse.splice(index, 1)
+      } else if (tableName === 'selectiveCourse') {
+        this.selectiveCourse.splice(index, 1)
+      } else if (tableName === 'commonCourse') {
+        this.commonCourse.splice(index, 1)
+      }
+    },
+    addCourse: function (index, tableName) {
     }
   }
 }
