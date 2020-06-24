df54e6f39669c1c13a3dbeeb8bb474befab853a0
SolarBear251 <948697775@qq.com>
2020-06-24 17:01:24 +0800

    fix: bug

--- src/components/ProgramView.vue
+++ src/components/ProgramView.vue
@@ -19,7 +19,8 @@
         <el-table-column prop="credit" label="学分" align="center"></el-table-column>
         <el-table-column label="操作" align="center">
           <template slot-scope="scope">
-            <el-button type="danger" @click="delTableRow(scope.$index, 'requiredCourse')" disabled="true">删除</el-button>
+            <el-button type="danger" @click="delTableRow(scope.$index, 'requiredCourse')"
+                       :disabled="true">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
